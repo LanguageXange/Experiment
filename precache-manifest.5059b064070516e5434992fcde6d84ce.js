0eea32ab985905ea3c64639a8139fabbff2b16a8
LanguageXange <hlin0017@seattlecentral.edu>
2020-07-14 17:15:13 +0800

    Updates

---
+++ precache-manifest.5059b064070516e5434992fcde6d84ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38805d785a592796d2cae579302ce126",
+    "url": "/LanguageXange/Experiment/index.html"
+  },
+  {
+    "revision": "e2efebee6c96993dad4f",
+    "url": "/LanguageXange/Experiment/static/css/main.42b59cf5.chunk.css"
+  },
+  {
+    "revision": "abd4ba65c8b5925c3de0",
+    "url": "/LanguageXange/Experiment/static/js/2.d2eabe9c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/LanguageXange/Experiment/static/js/2.d2eabe9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2efebee6c96993dad4f",
+    "url": "/LanguageXange/Experiment/static/js/main.345b2857.chunk.js"
+  },
+  {
+    "revision": "a6502ca72a6e93847539",
+    "url": "/LanguageXange/Experiment/static/js/runtime-main.0fc29384.js"
+  },
+  {
+    "revision": "29380e8ba75272ab2b28ecf8757b9b6e",
+    "url": "/LanguageXange/Experiment/static/media/laptop2.29380e8b.jpg"
+  }
+]);
